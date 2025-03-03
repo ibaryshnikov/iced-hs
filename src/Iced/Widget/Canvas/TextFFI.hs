9ef5f56e104aced5273bf2e751859c5a437ecd4d
ibaryshnikov <baryshnikov.il@gmail.com>
2025-03-04 02:48:13 +0300

    formatting, removed comment

--- src/Iced/Widget/Canvas/TextFFI.hs
+++ src/Iced/Widget/Canvas/TextFFI.hs
@@ -20,17 +20,6 @@ import Iced.Widget.Canvas.Text
 data NativeText
 type TextPtr = Ptr NativeText
 
--- content_ptr: *mut c_char,
--- position_x: c_float,
--- position_y: c_float,
--- color_ptr: *mut Color,
--- size: c_float,
--- line_height_ptr: *mut LineHeight,
--- horizontal_alignment_ptr: *mut Alignment,
--- vertical_alignment_ptr: *mut Alignment,
--- shaping_raw: c_uchar,
-
--- content position_x position_y color size
 foreign import ccall "canvas_text_new"
   canvas_text_new
     :: CString -- content
@@ -53,4 +42,13 @@ newCanvasText Text{lineHeight = textLineHeight, ..} = do
   lineHeightPtr <- valueToNativeIO textLineHeight
   horizontalRaw <- valueToNativeIO horizontal
   verticalRaw <- valueToNativeIO vertical
-  canvas_text_new contentRaw (CFloat x) (CFloat y) colorPtr (CFloat size) lineHeightPtr horizontalRaw verticalRaw (valueToNative shaping)
+  canvas_text_new
+    contentRaw
+    (CFloat x)
+    (CFloat y)
+    colorPtr
+    (CFloat size)
+    lineHeightPtr
+    horizontalRaw
+    verticalRaw
+    (valueToNative shaping)
